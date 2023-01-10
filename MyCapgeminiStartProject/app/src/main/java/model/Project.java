2d2e20d2e4b4b8a8f66977a83bb1e08f8f355a4a
pacasarini <paulacasarini@hotmail.com>
2023-01-10 14:31:58 -0300

    created classes for the projects

---
+++ MyCapgeminiStartProject/app/src/main/java/model/Project.java
@@ -0,0 +1,78 @@
+
+package model;
+
+import java.util.Date;
+/**
+ *
+ * @author Paula
+ */
+public class Project {
+    
+    private int id;
+    private String name;
+    private String description;
+    private Date createdAt; // Date is not native, it needs to be imported
+    private Date updatedAt; 
+    
+    //alt + insert para gerar o contrutor:
+    public Project(int id, String name, String description, Date createdAt, Date updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    //alt + insert para gerar os getters and setters:
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    
+    
+    //alt + insert para gerar o método toString():
+
+    @Override
+    public String toString() {
+        return "Project{" + "id=" + id + ", name=" + name + ", description=" + description + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + '}';
+    }
+    
+    
+    
+}
